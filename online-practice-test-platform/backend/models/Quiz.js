25fc16be0e789c40c107be219e5553af60b686fe
harsh-1201 <harshalc070@gmail.com>
2024-05-19 16:36:58 +0530

    Update Quiz.js

--- online-practice-test-platform/backend/models/Quiz.js
+++ online-practice-test-platform/backend/models/Quiz.js
@@ -1,1 +1,9 @@
+const mongoose = require('mongoose');
+const QuizSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  questions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Question' }],
+  score: Number,
+  createdAt: { type: Date, default: Date.now }
+});
+module.exports = mongoose.model('Quiz', QuizSchema);
 
