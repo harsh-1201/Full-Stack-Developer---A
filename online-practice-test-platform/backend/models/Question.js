1ced133fca76e16268d635ecb0b8d5f6b92ab67a
harsh-1201 <harshalc070@gmail.com>
2024-05-19 16:43:18 +0530

    Update Question.js

--- online-practice-test-platform/backend/models/Question.js
+++ online-practice-test-platform/backend/models/Question.js
@@ -1,1 +1,35 @@
+// backend/models/Question.js
+
+const mongoose = require('mongoose');
+
+const QuestionSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  options: [
+    {
+      text: {
+        type: String,
+        required: true,
+      },
+      isCorrect: {
+        type: Boolean,
+        required: true,
+      },
+    },
+  ],
+  difficulty: {
+    type: Number,
+    required: true,
+  },
+  tags: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+});
+
+module.exports = mongoose.model('Question', QuestionSchema);
 
